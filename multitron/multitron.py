08863293bdd90328846b532f67aea47ea9d20180
knowsWhereHisTowelIs <cscalebslater@gmail.com>
2016-05-23 19:49:21 -0400

    Multitron Comments + basic design

--- multitron/multitron.py
+++ multitron/multitron.py
@@ -1,6 +1,7 @@
 import random, pickle
 import numpy as np
 import matplotlib
+import perceptron
 
 def stepFunc(x):
     """Typical Step function U(t)"""
@@ -14,7 +15,7 @@ class Multitron(object):
     which number it is
     (28x28=784)
     """
-
+    numOfCharacters = 10
     iterations = 200
     learningRate = .2
 
@@ -32,6 +33,22 @@ class Multitron(object):
         return
 
     def loadData(self):
+        """
+        Load part of the MNIST data
+        3 parts
+        training data  10,000 elements
+            2D array (
+                array( 28x28 bitmap ), correctNumber[0-9]
+            )
+        validation data 1,000 elements
+            2D array (
+                array( 28x28 bitmap ), correctNumber[0-9]
+            )
+        testing data 1,000 elements
+            2D array (
+                array( 28x28 bitmap ), correctNumber[0-9]
+            )
+        """
         with open('./littlemnist.pkl', 'rb') as f:
             (trainX, trainY), (validX, validY), (testX, testY) = pickle.load(f,encoding= 'latin1')
         print('loaded data')
@@ -39,32 +56,53 @@ class Multitron(object):
         self.validData = [(x,y)for x,y in zip (validX, validY)]
         self.testingData =  [(x,y)for x,y in zip (testX, testY)]
 
+        # print( "TRAINING", len(self.trainingData), "VALID", len(self.validData), "TESTING", len(self.testingData) )
+        # exit(0);
+
         # array of perceptrons for each number
-        self.perceptrons = []
+        self.perceptrons = perceptron.create( self.numOfCharacters )
         # print(self.trainingData[0][0].size)
-        self.weights = np.random.rand( 10 )
+        self.weights = np.random.rand( self.numOfCharacters )
         return
 
     def train(self):
-        for i in range(self.iterations):
-            #choose random row from training data
-            inputRow, isTrue = random.choice(self.trainingData)
+        """
+        Use training data to update weights
+        """
+        numOfTrainingData = len( self.trainingData )
+        numOfValidData = len( self.validData )
 
-            # multiply trainingData subarray by weights then sum answer
-            result = np.dot(self.weights, inputRow)
-            # update error?
-            error = isTrue - unitStep(result)
-            # update weights
-            self.weights += learningRate * error * inputRow
+        # loop through all training data
+        for i in range(numOfTrainingData):
+            # train each character with training data row
+            for j in range( self.numOfCharacters ):
+                self.perceptrons[j].train( self.trainingData[i][0], self.trainingData[i][1] )
+
+            # every divisable peice of training data
+            # redo weights with valid data
+            if( (i % numOfValidData) === 0 ):
+                for j in range( self.numOfCharacters ):
+                    # TODO figure out what has to be done for validation
+
         return
 
     def validate(self):
+        """
+        Rerun training with known values to update weights
+        ??? should be run every inbetween 'x' peices of training data
+        """
         return
 
     def test(self):
+        """
+        go through testing data and show which ones output correct weights
+        """
         return
 
     def printTrainingDataInfo(self):
+        """
+        TODO delete? just debug... not useful right now
+        """
         print("test[0]:{} \
             \nlen(test[0])={}\
             \nlen(test[0][])={}\
---
+++ multitron/perceptron.py
@@ -0,0 +1,79 @@
+from random import choice
+# from numpy import array, dot, random
+import numpy as np
+
+def unitStep(x):
+    """ 1 if > 0 else 0 """
+    return 1 if x > 0 else 0
+
+def create( n = 1 ):
+    """
+    create 'n' perceptrons and return array
+    """
+    perceptrons = []
+    for i in range(n):
+        perceptrons.append( perceptron() )
+    return perceptrons
+
+class perceptron(object):
+    """
+    Learning algorithm
+    supply me with training DATA
+    update weights
+    "Learn" weights needed for correct answer
+    """
+
+    learningRate = 0.2
+    # TODO remove??? dont think needed since multitron runs?
+    interations = 100
+
+    def __init__(self):
+        """
+        create variables
+        set
+        """
+        self.weights = np.random.rand(10)#np.random.rand(weights)
+
+        return
+
+    def train(self, inputRow, isInputTrue):
+        """
+        Update weights based on training data
+        inputRow - the training data
+        isInputTrue - boolean is training data correct
+        """
+        # multiply trainingData subarray by weights then sum answer
+        result = np.dot(self.weights, inputRow)
+        # update error?
+        error = isInputTrue - unitStep(result)
+        # update weights
+        self.weights += self.learningRate * error * inputRow
+
+        return
+
+    def initializeWeights(self, trainingData):
+        """
+        TODO delete??? don't use
+        """
+        for i in range(iterations):
+            #choose random row from training data
+            inputRow, isTrue = random.choice(trainingData)
+
+            # multiply trainingData subarray by weights then sum answer
+            result = np.dot(self.weights, inputRow)
+            # update error?
+            error = isTrue - unitStep(result)
+            # update weights
+            self.weights += self.learningRate * error * inputRow
+        return
+
+    def outputResults(data):
+        """
+        TODO finish?
+        """
+        # output learned results
+        # inputRow, ignored
+        # for inputRow, _ in data:
+        #         result = dot(inputRow, weights)
+        #         print("{}: {} -> {}".format(inputRow, result, unitStep(result) ))
+        return
